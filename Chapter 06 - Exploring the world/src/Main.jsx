02affe00972bc07353036d8b7ccca40903ea0486
Reddyveerendra <reddyveerendra535@gmail.com>
2024-01-21 14:52:24 +0530

    Exploring the world project

---
+++ Chapter 06 - Exploring the world/src/Main.jsx
@@ -0,0 +1,47 @@
+import { imgPrefix } from "./config";
+export const Main = (props) => {
+  return (
+    <div className="restaurants">
+      {props.data.map((item) => (
+        <div key={item.name} className="cards">
+          <div className="image-container">
+            <img
+              className="background-image"
+              src={imgPrefix + item.cloudinaryImageId}
+              alt={item.name}
+            />
+            <div className="text-overlay">
+              <div className="text-background">
+                <h2>{item.aggregatedDiscountInfoV3.header}</h2>
+                <p>{item.aggregatedDiscountInfoV3.subHeader}</p>
+              </div>
+            </div>
+          </div>
+          <div className="info">
+            <h5>
+              {item.name.length <= 20
+                ? item.name
+                : item.name.slice(0, 17) + "..."}
+            </h5>
+            <div>
+              <p>
+                <i className="bi bi-star-fill star"></i>
+                {" " +
+                  item.avgRatingString +
+                  " . " +
+                  item.sla.deliveryTime +
+                  " mins"}
+              </p>
+            </div>
+            <h6>
+              {item.cuisines.join(",") <= 25
+                ? item.cuisines.join(",")
+                : item.cuisines.join(",").slice(0, 22) + "..."}
+            </h6>
+            <h6>{item.areaName}</h6>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
